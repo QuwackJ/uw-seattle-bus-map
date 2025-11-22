2740244bade8ba8c8afeca887dfa256d88ce0171
QuwackJ <quwackj@hotmail.com>
2025-11-21 20:32:54 -0800

    added basic comments to explain menu_logic and live_bus_positions

--- menu_logic.js
+++ menu_logic.js
@@ -1,5 +1,7 @@
+// importing static route data for lookups
 import { routesById } from "./routes_uw_static.js";
 
+// menu logic for route selection and stop fading
 export function menuLogic(map) {
     const routeButtons = document.querySelectorAll('.route-btn');
     let activeRouteId = null;
@@ -8,6 +10,7 @@ export function menuLogic(map) {
         const route = routesById[route_id];
         const ids = new Set();
 
+        // find all stops for a route in both directions
         for (const direction of ["0", "1"]) {
             const directionInfo = route.directions?.[direction];
 
@@ -24,15 +27,17 @@ export function menuLogic(map) {
     function fadeUnselectedRoutes(routeId) {
         const stopIds = getStopsIdsforRoute(routeId);
 
+        // set opacity for stops along unselected routes
         map.setPaintProperty('uw-stops', 'circle-opacity', [
             'case',
             ['in', ['get', 'stop_id'], ['literal', stopIds]],
-            1,
+            1, // stops on a selected route are fully visible
             0.2
         ]);
     }
 
     function resetStopOpacity() {
+        // reset stop opacity to fully visible for all stops when no route is selected
         map.setPaintProperty('uw-stops', 'circle-opacity', 1);
     }
 
@@ -40,6 +45,7 @@ export function menuLogic(map) {
         b.addEventListener('click', () => {
             const routeId = b.getAttribute('data-route-id');
 
+            // when the same route is selected again, reset the opacity of bus stops (map should go back to default state)
             if (activeRouteId === routeId) {
                 resetStopOpacity();
                 activeRouteId = null;
@@ -47,6 +53,7 @@ export function menuLogic(map) {
                 return;
             }
 
+            // when a route is selected, fade bus stops that are not along selected route
             activeRouteId = routeId;
             fadeUnselectedRoutes(routeId);
             routeButtons.forEach(btn => btn.classList.remove('active'));
